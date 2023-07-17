4b0d87866809e1d0e5386ea71465cc6494f380cc
Sean <Seanpf12@hotmail.com>
2023-07-17 11:36:11 -0500

    init of Tag related files

--- src/main/java/com/launchcode/recipeproject/controllers/TagsController.java
+++ src/main/java/com/launchcode/recipeproject/controllers/TagsController.java
@@ -1,4 +1,12 @@
 package com.launchcode.recipeproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("tags")
 public class TagsController {
+
+
 }
