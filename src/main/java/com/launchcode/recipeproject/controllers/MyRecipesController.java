f7b52fba5cfb2b6547f193b5094d788c78b0e20d
kelseyjungermann <kelsey.jungemrann@gmail.com>
2023-08-12 11:22:00 -0500

    created controller for myRecipes

---
+++ src/main/java/com/launchcode/recipeproject/controllers/MyRecipesController.java
@@ -0,0 +1,27 @@
+package com.launchcode.recipeproject.controllers;
+
+import com.launchcode.recipeproject.data.RecipeRepository;
+import com.launchcode.recipeproject.data.UserRepository;
+import com.launchcode.recipeproject.models.User;
+import com.launchcode.recipeproject.models.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
+
+@Controller
+public class MyRecipesController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @GetMapping("/profile/myRecipes")
+    public String displayMyRecipes(Model model){
+
+        return "/profile/myRecipes";
+    }
+}
