ff4b26aa62f79f4aca9a554193be4bac6d6d5b52
jenbilyeu80 <125934039+jenbilyeu80@users.noreply.github.com>
2023-08-18 23:42:56 -0500

    MySearchBar

--- src/main/java/com/launchcode/recipeproject/data/FavoriteRepository.java
+++ src/main/java/com/launchcode/recipeproject/data/FavoriteRepository.java
@@ -2,9 +2,14 @@ package com.launchcode.recipeproject.data;
 
 import com.launchcode.recipeproject.models.Favorite;
 import com.launchcode.recipeproject.models.Ingredient;
+import com.launchcode.recipeproject.models.Recipe;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FavoriteRepository extends CrudRepository<Favorite, Integer> {
+
+    List<Favorite> findByUserId(Integer id);
 }
